9c0beca09b9ce336fb50bea518a1d01a7078a4ca
Tetsufun5 <v1c.firth1235@gmail.com>
2025-06-16 16:35:32 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//ボタンをクリックした際のアニメーションの設定
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $(".gnav").toggleClass('panelactive');//ヘッダーにpanelactiveクラスを付与
+});
+$("#g-navi li a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".header").removeClass('panelactive');//ヘッダーのpanelactiveクラスも除去
+    $(".btn").removeClass('active');//ボタンの activeクラスを除去し
+});
